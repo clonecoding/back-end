8c16577da06ec0c2e14b541e77e31ce3e45af927
jeonghyeonuk <junghunwook456@naver.com>
2022-06-17 20:02:06 +0900

    S3 설정

--- src/main/java/com/sparta/clonecoding/ClonecodingApplication.java
+++ src/main/java/com/sparta/clonecoding/ClonecodingApplication.java
@@ -1,15 +1,19 @@
 package com.sparta.clonecoding;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ClonecodingApplication {
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(ClonecodingApplication.class, args);
+        new SpringApplicationBuilder(ClonecodingApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
-
 }
