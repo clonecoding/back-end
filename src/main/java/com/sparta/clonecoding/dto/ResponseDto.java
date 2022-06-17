9d400f0ee6161b327cd5f7caf980165e4c5d56ee
jeonghyeonuk <junghunwook456@naver.com>
2022-06-17 17:18:46 +0900

    security 부분 생성

---
+++ src/main/java/com/sparta/clonecoding/dto/ResponseDto.java
@@ -0,0 +1,37 @@
+package com.sparta.clonecoding.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Setter@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseDto<T> {
+    private boolean response;
+    private String message;
+    private List<T> postList;
+
+    private String token;
+
+    public ResponseDto(boolean response, String message, String token) {
+        this.response = response;
+        this.message = message;
+        this.token = token;
+    }
+
+    public ResponseDto(boolean response, String message) {
+        this.response = response;
+        this.message = message;
+    }
+
+    public ResponseDto(boolean response, String message, List<T> postList) {
+        this.response = response;
+        this.message = message;
+        this.postList = postList;
+    }
+}
