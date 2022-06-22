9d308e30b88de45deabf9a9a5472229bc887ae2c
jeonghyeonuk <junghunwook456@naver.com>
2022-06-22 09:47:21 +0900

    웹소켓(stomp)생성

--- src/main/java/com/sparta/clonecoding/socket/WebSocketChatHandler.java
+++ src/main/java/com/sparta/clonecoding/socket/WebSocketChatHandler.java
@@ -1,7 +1,6 @@
 package com.sparta.clonecoding.socket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sparta.clonecoding.socket.ChatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -14,16 +13,13 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class WebSocketChatHandler extends TextWebSocketHandler {
     private final ObjectMapper objectMapper;
-    private final ChatService chatService;
+
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-// 삭제        TextMessage textMessage = new TextMessage("Welcome chatting sever~^^ ");
-// 삭제       session.sendMessage(textMessage);
-        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
-        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
-        room.handleActions(session, chatMessage, chatService);
+      TextMessage textMessage = new TextMessage("Welcome chatting sever~^^ ");
+      session.sendMessage(textMessage);
     }
 }
